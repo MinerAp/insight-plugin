6fddda10766bedaa380589124abbd91c68b5e0c2
Andrew Shulman <andy.shulman@hotmail.com>
2014-09-27 18:36:58 -0400

    Don't implement AutoCloseable because this is merely a view of a connection. The underlying connection needs to be handled seperately.

--- src/main/java/com/amshulman/insight/backend/AbstractCallbackReadBackend.java
+++ src/main/java/com/amshulman/insight/backend/AbstractCallbackReadBackend.java
@@ -10,17 +10,12 @@ import lombok.RequiredArgsConstructor;
 import com.amshulman.insight.query.QueryParameterBuilder;
 
 @RequiredArgsConstructor
-public abstract class AbstractCallbackReadBackend implements AutoCloseable {
+public abstract class AbstractCallbackReadBackend {
 
     protected static final ExecutorService threadPool = new ThreadPoolExecutor(5, 10, 1, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(100));
     protected final ReadBackend readBackend;
 
     public final QueryParameterBuilder newQueryBuilder() {
         return readBackend.newQueryBuilder();
-    }
-
-    @Override
-    public final void close() {
-        readBackend.close();
     }
 }
