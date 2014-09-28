b10bbadec3ec2a140d911e992cd6dc3332132d9a
Andrew Shulman <andy.shulman@hotmail.com>
2014-09-27 20:38:47 -0400

    Fix block below sapling/mushroom being logged during grow events and add rollback data

--- src/main/java/com/amshulman/insight/event/block/StructureGrowListener.java
+++ src/main/java/com/amshulman/insight/event/block/StructureGrowListener.java
@@ -1,5 +1,7 @@
 package com.amshulman.insight.event.block;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.BlockState;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -9,6 +11,7 @@ import com.amshulman.insight.event.InternalEventHandler;
 import com.amshulman.insight.row.BlockRowEntry;
 import com.amshulman.insight.types.EventCompat;
 import com.amshulman.insight.util.NonPlayerLookup;
+import com.amshulman.insight.util.Util;
 
 public class StructureGrowListener extends InternalEventHandler<StructureGrowEvent> {
 
@@ -18,9 +21,20 @@ public class StructureGrowListener extends InternalEventHandler<StructureGrowEve
 
         String name = event.getPlayer() == null ? NonPlayerLookup.NATURE : event.getPlayer().getName();
         long time = System.currentTimeMillis();
+        Location base = event.getLocation().subtract(0, 1, 0);
 
         for (BlockState b : event.getBlocks()) {
-            add(new BlockRowEntry(time, name, EventCompat.BLOCK_GROW, b));
+            BlockState previousState = Util.getBlockStateOrNullIfAir(b.getBlock().getState());
+
+            if (b.getLocation().equals(base)) {
+                if (previousState.getType() != Material.DIRT || previousState.getRawData() != 0) {
+                    add(new BlockRowEntry(System.currentTimeMillis(), NonPlayerLookup.NATURE, EventCompat.BLOCK_DIE, previousState, b));
+                }
+
+                continue;
+            }
+
+            add(new BlockRowEntry(time, name, EventCompat.BLOCK_GROW, b, previousState));
         }
     }
 }
