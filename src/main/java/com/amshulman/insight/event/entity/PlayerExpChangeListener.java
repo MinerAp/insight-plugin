564df8f4ea64fc1603e874a06a04a4ec39fa5fa1
Andrew Shulman <andy.shulman@hotmail.com>
2017-03-10 02:41:20 -0500

    Fix hardcoded entity name

--- src/main/java/com/amshulman/insight/event/entity/PlayerExpChangeListener.java
+++ src/main/java/com/amshulman/insight/event/entity/PlayerExpChangeListener.java
@@ -8,12 +8,13 @@ import org.bukkit.event.player.PlayerExpChangeEvent;
 import com.amshulman.insight.event.InternalEventHandler;
 import com.amshulman.insight.row.EntityRowEntry;
 import com.amshulman.insight.types.EventCompat;
+import com.amshulman.insight.util.EntityUtil;
 
 public class PlayerExpChangeListener extends InternalEventHandler<PlayerExpChangeEvent> {
 
     @Override
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void listen(PlayerExpChangeEvent event) {
-        add(new EntityRowEntry(System.currentTimeMillis(), event.getPlayer().getName(), EventCompat.EXP_GAIN, event.getPlayer().getLocation(), EntityType.EXPERIENCE_ORB.toString()));
+        add(new EntityRowEntry(System.currentTimeMillis(), event.getPlayer().getName(), EventCompat.EXP_GAIN, event.getPlayer().getLocation(), EntityUtil.getName(EntityType.EXPERIENCE_ORB)));
     }
 }
