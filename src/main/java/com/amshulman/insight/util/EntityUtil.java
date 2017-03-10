d5466761285c1861af752a0687432620b41b2b77
Andrew Shulman <andy.shulman@hotmail.com>
2017-03-10 02:08:25 -0500

    Generate most entity names automatically

--- src/main/java/com/amshulman/insight/util/EntityUtil.java
+++ src/main/java/com/amshulman/insight/util/EntityUtil.java
@@ -3,6 +3,7 @@ package com.amshulman.insight.util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import org.apache.commons.lang.WordUtils;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -22,86 +23,27 @@ public final class EntityUtil {
 
     public static String getName(EntityType type) {
         switch (type) {
-            case CREEPER:
-                return "Creeper";
-            case SKELETON:
-                return "Skeleton";
-            case SPIDER:
-                return "Spider";
-            case GIANT:
-                return "Giant";
-            case ZOMBIE:
-                return "Zombie";
-            case SLIME:
-                return "Slime";
-            case GHAST:
-                return "Ghast";
-            case PIG_ZOMBIE:
-                return "Zombie Pigman";
-            case ENDERMAN:
-                return "Enderman";
-            case CAVE_SPIDER:
-                return "Cave Spider";
-            case SILVERFISH:
-                return "Silverfish";
-            case BLAZE:
-                return "Blaze";
-            case MAGMA_CUBE:
-                return "Magma Cube";
-            case ENDER_DRAGON:
-                return "Ender Dragon";
-            case WITHER:
-                return "Wither";
-            case BAT:
-                return "Bat";
-            case WITCH:
-                return "Witch";
-            case ENDERMITE:
-                return "Endermite";
-            case GUARDIAN:
-                return "Guardian";
-            case SHULKER:
-                return "Shulker";
-            case PIG:
-                return "Pig";
-            case SHEEP:
-                return "Sheep";
-            case COW:
-                return "Cow";
-            case CHICKEN:
-                return "Chicken";
-            case SQUID:
-                return "Squid";
-            case WOLF:
-                return "Wolf";
-            case MUSHROOM_COW:
-                return "Mooshroom";
-            case SNOWMAN:
-                return "Snowman";
-            case OCELOT:
-                return "Ocelot";
-            case IRON_GOLEM:
-                return "Iron Golem";
-            case HORSE:
-                return "Horse";
-            case RABBIT:
-                return "Rabbit";
-            case POLAR_BEAR:
-                return "Polar Bear";
-            case VILLAGER:
-                return "Villager";
-            case ENDER_CRYSTAL:
-                return "Ender Crystal";
-            case PRIMED_TNT:
-            case MINECART_TNT:
-                return "TNT";
-            case EXPERIENCE_ORB:
-                return "XP Orb";
-            case FALLING_BLOCK:
-                return "Falling Block";
-            default:
-                System.out.println("EntityUtil - ??? " + type.name());
-                return type.getName();
+          case PIG_ZOMBIE:
+              return "Zombie Pigman";
+          case MUSHROOM_COW:
+              return "Mooshroom";
+          case PRIMED_TNT:
+          case MINECART_TNT:
+              return "TNT";
+          case EXPERIENCE_ORB:
+              return "XP Orb";
+          case MINECART_COMMAND:
+              return "Command Block Minecart";
+          case MINECART_CHEST:
+              return "Chest Minecart";
+          case MINECART_FURNACE:
+              return "Furnace Minecart";
+          case MINECART_HOPPER:
+              return "Hopper Minecart";
+          case MINECART_MOB_SPAWNER:
+              return "Mob Spawner Minecart";
+          default:
+            return WordUtils.capitalizeFully(type.name().replace('_', ' '));
         }
     }
 }
